ec4cb9958d27733d022ec53ead2bad70d105ffd4
Sanskar Srivastava <113485640+sanskarsri26@users.noreply.github.com>
2025-04-06 15:04:38 -0700

    chnages

---
+++ submission.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Xml.Schema;
+using System.Xml;
+using Newtonsoft.Json;
+using System.IO;
+
+
+
+/**
+ * This template file is created for ASU CSE445 Distributed SW Dev Assignment 4.
+ * Please do not modify or delete any existing class/variable/method names. However, you can add more variables and functions.
+ * Uploading this file directly will not pass the autograder's compilation check, resulting in a grade of 0.
+ * **/
+
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static string xmlURL = "https://sanskarsri26.github.io/assignment4-445/Hotels.xml";
+        public static string xmlErrorURL = "https://sanskarsri26.github.io/assignment4-445/HotelsErrors.xml";
+        public static string xsdURL = "https://sanskarsri26.github.io/assignment4-445/Hotels.xsd";
+
+        public static void Main(string[] args)
+        {
+            string result = Verification(xmlURL, xsdURL);
+            Console.WriteLine(result);
+
+
+            result = Verification(xmlErrorURL, xsdURL);
+            Console.WriteLine(result);
+
+
+            result = Xml2Json(xmlURL);
+            Console.WriteLine(result);
+        }
+
+        // Q2.1
+        public static string Verification(string xmlUrl, string xsdUrl)
+        {
+        try
+            {
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.Schemas.Add(null, xsdUrl);
+                settings.ValidationType = ValidationType.Schema;
+
+                string errorMessage = "No Error";
+
+                settings.ValidationEventHandler += (sender, args) =>
+                {
+                    errorMessage = args.Message;
+                };
+
+                using (XmlReader reader = XmlReader.Create(xmlUrl, settings))
+                {
+                    while (reader.Read()) { }
+                }
+
+                return errorMessage;
+            }
+            catch (Exception ex)
+            {
+                return $"Exception: {ex.Message}";
+            }
+        }
+
+        public static string Xml2Json(string xmlUrl)
+        {          
+            try
+            {
+                using (XmlReader reader = XmlReader.Create(xmlUrl))
+                {
+                    XmlDocument doc = new XmlDocument();
+                    doc.Load(reader);
+                    string jsonText = JsonConvert.SerializeXmlNode(doc, Newtonsoft.Json.Formatting.Indented, true);
+                    return jsonText;
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"Exception: {ex.Message}";
+            }
+        }
+    }
+
+}
